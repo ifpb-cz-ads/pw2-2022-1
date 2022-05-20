b46e9c7982c62ee5f2f359a63d8650c7f18c61ca
Paulo Ewerton <pauloewerton@icloud.com>
2022-05-20 16:57:23 -0300

    Aula 08

--- aula08/app.js
+++ aula08/app.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
+const db = require('./config/db.config');
+const messageController = require('./controllers/message');
 
 const app = express();
 
 app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// sincroniza o bd
+db.sync();
 // cria um vetor global para conter as entradas
 let entries = [];
 // torna o vetor acessível em todas as views
-app.locals.entries = entries;
+app.locals.entries = entries.concat(messageController.messageList());
 
 // inicializa o logger com nível dev
 app.use(logger('dev'));
@@ -19,7 +23,6 @@ app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 
 // definição dos roteadores (rotas)
-
 app.get('/', (request, response) => {
     // views/index.ejs
     response.render('index');
@@ -31,7 +34,7 @@ app.get('/new-entry', (request, response) => {
 
 app.post('/new-entry', (request, response) => {
     if (!request.body.title || !request.body.body) {
-        resonse.status(400).send('As postagens devem ter um título de um corpo.');
+        response.status(400).send('As postagens devem ter um título e um corpo.');
         return;
     }
 
