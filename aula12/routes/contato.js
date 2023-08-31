d40f1fada4d03972086a4a88ae61e8a275006f55
Paulo Ewerton <pauloewerton@icloud.com>
2023-08-31 16:23:23 -0300

    Removing authentication from contato routes

--- aula12/routes/contato.js
+++ aula12/routes/contato.js
@@ -7,8 +7,8 @@ const contatoController = require('../controllers/contatoController');
 
 router.get('/', contatoController.contatosList);
 router.get('/:id', contatoController.contatosRead);
-router.post('/', verify, contatoController.contatosCreate);
-router.put('/:id', verify, contatoController.contatosUpdate);
-router.delete('/:id', verify, contatoController.contatosDelete);
+router.post('/', contatoController.contatosCreate);
+router.put('/:id', contatoController.contatosUpdate);
+router.delete('/:id', contatoController.contatosDelete);
 
 module.exports = router;
